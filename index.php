c8c874f64635c9bcb965bc2115adf150a1e72d8b
Mounir Isly <33634696+MounirIsly@users.noreply.github.com>
2018-10-26 19:55:01 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include ('inc/connection.php'); 
+include ('inc/header.php');
+?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Time Tracker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+    <div class="main-list">
+   <p><a href="project.php">Add Project</a></p>
+   <p><a href="task.php">Add Task</a></p>
+   <p><a href="svg.php">Reports</a></p>
+   
+</div>
+</body>
+</html>
