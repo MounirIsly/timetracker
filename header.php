9a6ef01f126676a1beee3a1611283515601f1079
Mounir Isly <33634696+MounirIsly@users.noreply.github.com>
2018-10-26 20:01:43 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+include('connection.php');
+
+?>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+
+<div class="wrap">
+<div class="main-nav">
+<nav>
+<span><a href="index.php">
+
+<h1>Time Tracker</h1>
+</a></span>
+<div class="svg"> 
+
+</div>
+<ul>
+
+<li><a href="task_list.php" >Tasks</a></li>
+
+
+<li><a href="project_list.php">Projects</a></li>
+<li><a href="reports.php">Reports</a></li>
+</ul>
+</nav>
+
+</div>
+
